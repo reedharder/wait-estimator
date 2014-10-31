d6e57c895f77719f810f889634aa69fa6ecc5ab7
Reed Harder <reedharder@gmail.com>
2014-10-31 01:34:46 -0400

    site frontend looking nice, exept scenarios

--- waitestapp/urls.py
+++ waitestapp/urls.py
@@ -6,8 +6,10 @@ urlpatterns = patterns('',
     url(r'^2/$', views.waitapp_aff, name='capacity'),
     url(r'^3/$', views.waitapp_attr, name='attributes'),
     url(r'^4/$', views.waitapp_contrule, name='contrules'),
-    #url(r'^5/$', views.waitapp_aff, name='delrules'),
-    #url(r'^5/$', views.waitapp_aff, name='delrules'),
-    url(r'^$', views.home_view, name='home'),    
+    url(r'^5/$', views.waitapp_delrule, name='delrules'),
+    url(r'^6/$', views.waitapp_utilization, name='utilization'),
+    url(r'^results/$', views.waitapp_results, name='results'),
+    url(r'^$', views.home_view, name='home'),
+    
 )
 
--- waitestapp/views.py
+++ waitestapp/views.py
@@ -14,3 +14,13 @@ def waitapp_attr(request):
     
 def waitapp_contrule(request):
     return render(request, 'waitestapp/waitapp_contrule.html')
+
+def waitapp_delrule(request):
+    return render(request, 'waitestapp/waitapp_delrule.html')
+
+def waitapp_utilization(request):
+    return render(request, 'waitestapp/waitapp_utilization.html')
+
+
+def waitapp_results(request):
+    return render(request, 'waitestapp/waitapp_results.html')
