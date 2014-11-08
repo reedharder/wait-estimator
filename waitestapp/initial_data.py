f9218fbb2ae6a27908dbf33209429d43e06c5c97
Reed Harder <reedharder@gmail.com>
2014-11-07 19:25:20 -0500

    sessions and json tables in progress

---
+++ waitestapp/initial_data.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  7 19:10:08 2014
+
+@author: Reed
+"""
+#OPEN TABLE IF CLOSED PERHAPS
+#CHECK IF NO PROCIDER ADDED HOPEFULLY
+capacity_json = [{"Provider Name":"Doctor 1","Hours Per Day":"8","Weeks Per Year":"48","Position":"Physician","Team":"Team A"},
+                 {"Provider Name":"Doctor 2","Hours Per Day":"8","Weeks Per Year":"48","Position":"Physician","Team":"Team A"},
+                 {"Provider Name":"Doctor 3","Hours Per Day":"8","Weeks Per Year":"50","Position":"Physician","Team":"Team B"},
+                 {"Provider Name":"Nurse 1","Hours Per Day":"8","Weeks Per Year":"50","Position":"Nurse Practitioner","Team":"Team B"},
+                 {"Provider Name":"Assist. 1","Hours Per Day":"8","Weeks Per Year":"50","Position":"Physician Assistant","Team":"Team A"}]
--- waitestapp/views.py
+++ waitestapp/views.py
@@ -1,23 +1,66 @@
 from django.shortcuts import render, render_to_response
 from django.contrib.formtools.wizard.views import SessionWizardView
 import waitestapp
+
+'''
+
 CURRENT_TEMPLATES={'capacity':'waitestapp/waitapp_capacity.html', 
                    'aff':'waitestapp/waitapp_aff.html',
                    'attr':'waitestapp/waitapp_attr.html',
                    'cont':'waitestapp/waitapp_contrule.html',
                    'del':'waitestapp/waitapp_delrule.html'}
+                   ## ADD SCENARIO TABS!
 
 CURRENT_FORMS = [("capacity", waitestapp.forms.CurrCapacityForm),
                  ("aff", waitestapp.forms.CurrAffForm),
                  ("attr", waitestapp.forms.CurrAttForm),
                  ("cont", waitestapp.forms.CurrContForm),
                  ("del", waitestapp.forms.CurrDelForm)]
-
+                 #ADD SCENARIO FORMS
+                 
+#Get list of physicians from capacity tab                 
+def get_physician_list(wizard):
+     # Get cleaned data from capacity step
+    cleaned_data = wizard.get_cleaned_data_for_step('capacity')
+    if cleaned_data:
+        phys_list=[record['Provider Name'] for record in cleaned_data['table'] if record['Position']=='Physician']
+    else:
+        phys_list = []
+    return phys_list
+    
+#Get list of physicians from capacity tab                 
+def get_nonphys_list(wizard):
+     # Get cleaned data from capacity step
+    cleaned_data = wizard.get_cleaned_data_for_step('capacity')
+    if cleaned_data:
+        phys_list=[record['Provider Name'] for record in cleaned_data['table'] if record['Position']!='Physician']
+    else:
+        phys_list = []
+    return phys_list
+        
 class CurrentWizard(SessionWizardView):
+    def get_template_names(self):
+        return [CURRENT_TEMPLATES[self.steps.current]]    
+        
+    def get_context_data(self, form, **kwargs):
+        context = super(CurrentWizard, self).get_context_data(form=form, **kwargs)
+        if self.steps.current == 'aff' or self.steps.current == 'attr':
+            phys_list=get_physician_list(self)
+            context.update({'phys_list': phys_list})
+        return context
+    
     def done(self, form_list, form_dict, **kwargs):
-        return render_to_response('waitestapp/waitapp_results.html', {
+        return render_to_response('waitestapp/waitapp_utilization.html', {
             'form_data': [form.cleaned_data for form in form_list],
         })
+
+
+wizard_view = CurrentWizard.as_view(CURRENT_FORMS)
+
+def current_wizard_view(request):
+    return wizard_view(request)
+'''
+
 
 def home_view(request):
     return render(request, 'waitestapp/base_site.html')
@@ -36,6 +79,7 @@ def waitapp_contrule(request):
 
 def waitapp_delrule(request):
     return render(request, 'waitestapp/waitapp_delrule.html')
+
 
 def waitapp_utilization(request):
     return render(request, 'waitestapp/waitapp_utilization.html')
